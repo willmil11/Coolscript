264126a75be478fd201f22d838ca6e480292a187
willmil11 <willmil11@gmail.com>
2023-12-28 12:48:10 +0100

    Added some base need fixes for the fix_dynamic function

--- v2/coolscript.py
+++ v2/coolscript.py
@@ -10,6 +10,7 @@ from time import perf_counter_ns
 import threading
 from os import path
 import os
+from subprocess import run as subprocess
 from dynamicprocessor import *
 from sys import exit
 from sys import argv as args
@@ -42,7 +43,47 @@ class messages:
     def version():
         print("v" + version)
 
+#Interpreter class
 class Interpreter:
+    class defaultfuncs:
+        def consolelog(*args):
+            print(*args)
+    #Create __init__ function that is ran when a new instance of the
+    #Interpreter class is created
+    def __init__(self):
+        #Init the interpreter
+        self.scopes = {
+            "global": {
+                "variables": {
+                    "console": {
+                        "type": "object",
+                        "value": {
+                            "log": {
+                                "type": "function",
+                                "functype": "native",
+                                "code": self.defaultfuncs.consolelog
+                            }
+                        }
+                    }
+                },
+                "subscopes": {
+                    "child": {
+                        "variables": {
+                            "hello": {
+                                "type": "string",
+                                "value": "Hello World!"
+                            }
+                        },
+                        "conditional": False,
+                        "parent": "global"
+                    }
+                },
+                "conditional": False,
+                "parent": None
+            }
+        }
+    #Create the dynamic_processor function that is used to do simple math
+    #and other stuff with strings
     def dynamic_processor(self, string):
         returns = None
         try:
@@ -50,11 +91,83 @@ class Interpreter:
         except Exception as e:
             raise Exception("Error while resolving dynamic data: " + str(e))
         return returns
-    
+    #Create fix_dynamic that is used to replace everything that is not a 
+    #string or a number before handing it to the dynamic_processor
+    def fix_dynamic(self, string, currentscope):
+        opened = False
+        for index, char in enumerate(string):
+            if char == "\"":
+                #If there's a char before the current char and its \ then ignore else invert opened
+                if index != 0:
+                    if string[index - 1] == "\\":
+                        continue
+                opened = not opened
+            else:
+                numbers = ["0","1","2","3","4","5","6","7","8","9"]
+                operators = ["+","-","*","/"]
+                #If not number or operator
+                if char not in numbers and char not in operators:
+                    if not opened:
+                        trim = string[index:]
+                        toreplace = {
+                            "charsindex": [],
+                            "toinsert": [],
+                            "trimstartindex": index
+                        }
+                        varname = ""
+                        for index, char in enumerate(trim):
+                            if char in numbers or char in operators or char == "\"" or char == "\\":
+                                break
+                            if char == " ":
+                                if varname == "":
+                                    continue
+                                else:
+                                    break
+                            varname += char
+                            toreplace["charsindex"].append(index)
+                        if varname == "":
+                            continue
+                        scan = self.scanscopevars(currentscope)
+                        if varname in scan.keys():
+                            toreplace["toinsert"] = scan[varname]
+                        else:
+                            raise Exception("Variable " + varname + " not found!")
+                        string = string[:toreplace["trimstartindex"]] + str(toreplace["toinsert"]) + string[toreplace["trimstartindex"] + len(varname):]
+        return string
+    #Scan the variables accessible by a scope with the scanscopevars 
+    #function
+    def scanscopevars(self, scope):
+        vars = {}
+
+        def scanscope(s):
+            # Add all variables from the current scope
+            vars.update(s['variables'])
+
+            # Check if the current scope has a parent scope
+            if s.get('parent'):
+                # Get the parent scope dictionary from the scopes dictionary
+                parent_scope = self.scopes[s['parent']]
+                # Recursively call scanscope with the parent scope
+                scanscope(parent_scope)
+
+            # Check if the current scope has any conditional child scopes
+            for child_scope in s.get('subscopes', {}).values():
+                if child_scope.get('conditional', False):
+                    # Recursively call scanscope with the conditional child scope
+                    scanscope(child_scope)
+
+        # Call scanscope with the initial scope
+        scanscope(scope)
+
+        return vars
+
+#Create an instance of the interpreter
 coolscript = Interpreter()
 
+print(coolscript.fix_dynamic("\"Hello world!\" + \"1\" + 2 + 1 + console", coolscript.scopes["global"]))
+
 #If arg is --help
-if args[1] == "--help":                                                                                     
+if args[1] == "--help":                                                                   
     messages.help()
     exit(0)
 else:
